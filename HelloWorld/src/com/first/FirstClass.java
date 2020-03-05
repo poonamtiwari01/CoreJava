06b5bc6e660594d5e4221a6507f76efbf82157b3
Poonam <Poonam@LAPTOP-N8KKOJBM>
2020-03-05 14:37:08 -0500

    added comments

--- HelloWorld/src/com/first/FirstClass.java
+++ HelloWorld/src/com/first/FirstClass.java
@@ -2,6 +2,6 @@ package com.first;
 
 public class FirstClass {
 	public static void main(String[] args) {
-		System.out.println("Hi  there  ! its my first JAVA program :) :( :)");
+		System.out.println("Hi  there  ! its my first JAVA program");
 	}
 }
--- HelloWorld/src/edu/tech/it/java/Exams.java
+++ HelloWorld/src/edu/tech/it/java/Exams.java
@@ -2,12 +2,14 @@ package edu.tech.it.java;
 
 public class Exams {
 	public int questions = 10;
+	// private variable
 	private int a = 9;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
 
+	// getters and setters
 	public int getA() {
 		return a;
 	}
